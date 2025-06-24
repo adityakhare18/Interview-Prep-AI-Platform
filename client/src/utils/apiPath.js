b149e3e81f397d651f8e8d1cc48efdcaa5cbffcf
Aditya Khare <122000208+adityakhare18@users.noreply.github.com>
2025-06-24 10:52:37 +0530

    Update apiPath.js

--- client/src/utils/apiPath.js
+++ client/src/utils/apiPath.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = https://interview-prep-ai-platform.onrender.com;
 
 export const API_PATH = {
     AUTH:{
