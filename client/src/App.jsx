32f26eb46563a2e1334b264eacb7ea9df6ea2d9a
adityakhare18 <khareaditya0105@gmail.com>
2025-06-28 09:13:59 +0530

    Change in APP.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,49 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Home/Dashboard";
 import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
-import UserProvider from "./context/userContext"
+import UserProvider, { UserContext } from "./context/userContext";
+import SpinnerLoader from "./components/Loader/SpinnerLoader";
+
+const AppRoutes = () => {
+  const { user, loading } = useContext(UserContext);
+
+  if (loading) {
+    return <SpinnerLoader />;
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="dashboard"
+          element={user ? <Dashboard /> : <Navigate to="/" replace />} />
+        <Route
+          path="interview-prep/:sessionId"
+          element={user ? <InterviewPrep /> : <Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+};
 
 const App = () => {
   return (
     <UserProvider>
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="interview-prep/:sessionId" element={<InterviewPrep />} />
-        </Routes>
-      </Router>
-
-      <Toaster
-        toastOptions={{
-          className: "",
-          style: {
-            fontSize: "13px",
-          },
-        }}
-      />
-    </div>
+      <div>
+        <AppRoutes />
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
     </UserProvider>
   );
 };
